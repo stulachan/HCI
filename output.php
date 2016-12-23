f2f93a31a3ea40849cbd64b0d3ce8a5d874c0cb3
netadmin <netadmin@netadmins-MacBook-Pro.local>
2016-12-23 12:48:22 -0800

    adding files

---
+++ output.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Confirm Page</title>
+        <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+        <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css'>
+        <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.0/css/bootstrapValidator.min.css'>
+
+        <meta charset="UTF-8">
+       
+    </head>
+    <body>
+       
+                
+                
+                
+   <table class="table table-hover" width="400">
+  <thead>
+    <tr>
+     
+      <th>Confirm your information</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+      <tr>
+          <td>First Name&nbsp;&nbsp; &nbsp; <?php echo $_POST["last_name"];?></td> 
+        
+    </tr>
+    
+    
+  </tbody>
+</table>
+                
+                
+                <nav aria-label="...">
+                    <ul class="pager">
+                    <li><a href="#">Previous</a></li>
+                    <li><a href="#">Confirm</a></li>
+                    </ul>
+                </nav>
+    </body>
+</html>
